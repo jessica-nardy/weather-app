dbd15bb4781d2f483453887f13fc0cee7c4c954b
jessica-nardy <nardy.arq@gmail.com>
2025-01-21 23:20:50 +0000

    Added axios

--- src/index.js
+++ src/index.js
@@ -3,7 +3,17 @@ function searchCity(event) {
   let input = document.querySelector("#city-input");
   let cityElement = document.querySelector("#current-city");
 
-  cityElement.innerHTML = input.value;
+  let cityName = input.value;
+  cityElement.innerHTML = cityName;
+
+  getWeather(cityName);
+}
+
+function getWeather(city) {
+  let apiKey = "8t7b43fadd5ba0704f357ba3a35e30o0";
+  let apiUrl = `"https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
 }
 
 let form = document.querySelector("#search-form");
