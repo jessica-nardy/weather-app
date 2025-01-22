297dc7e4b2c4d98621f1b32a5ee5247bfd71458e
jessica-nardy <nardy.arq@gmail.com>
2025-01-22 00:04:40 +0000

    Added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
-function searchCity(event) {
-  event.preventDefault();
-  let input = document.querySelector("#city-input");
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#current-city");
 
-  let cityName = input.value;
-  cityElement.innerHTML = cityName;
-
-  getWeather(cityName);
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
-function getWeather(city) {
+function searchCity(city) {
   let apiKey = "8t7b43fadd5ba0704f357ba3a35e30o0";
-  let apiUrl = `"https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
-  axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearch(event) {
+  event.preventDefault();
+  let input = document.querySelector("#city-input");
+
+  searchCity(input.value);
 }
 
 let form = document.querySelector("#search-form");
-form.addEventListener("submit", searchCity);
+form.addEventListener("submit", handleSearch);
+
+searchCity("Porto");
