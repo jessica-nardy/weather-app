3d32346b86c3f6a51ba9534f1e0aeef028f5c5da
jessica-nardy <nardy.arq@gmail.com>
2025-01-26 10:01:54 +0000

    Added forecast HTML from JavaScript

--- src/index.js
+++ src/index.js
@@ -48,6 +48,27 @@ function searchCity(city) {
   axios.get(apiUrl).then(refreshWeather);
 }
 
+function displayForecast() {
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `  <div class="weather-forecast-day">
+                    <div class="weather-forecast-date">Sun</div>
+                    <div class="weather-forecast-icon">🌤️</div>
+                    <div class="weather-forecast-temperatures">
+                        <div class="weather-forecast-temperature"><strong>15°</strong></div>
+                        <div class="weather-forecast-temperature">9°</div>
+                    </div>
+                </div>`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 function handleSearch(event) {
   event.preventDefault();
   let input = document.querySelector("#city-input");
@@ -59,3 +80,4 @@ let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSearch);
 
 searchCity("Porto");
+displayForecast();
